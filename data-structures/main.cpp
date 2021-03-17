94f0395c427306996eb226d32efd552b6acced0f
Miguel Lastras <mlastras@Miguels-MacBook-Pro-2.local>
2021-03-17 17:22:45 -0600

    Initial Commit

---
+++ data-structures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  data-structures
+//
+//  Created by Miguel Lastras on 3/17/21.
+//  Copyright © 2021 Miguel Lastras. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
